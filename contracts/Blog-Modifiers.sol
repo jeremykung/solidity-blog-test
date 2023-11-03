0607883dfaa184ad51b4963e260c26de333518d8
jeremykung <jeremykung@gmail.com>
2023-11-03 23:02:47 +0200

    initial commit

--- contracts/Blog-Modifiers.sol
+++ contracts/Blog-Modifiers.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.18;
 
 contract Blog {
 
+    address public owner = msg.sender;
     uint8 constant MAX_TITLE_LENGTH = 128;
-    uint16 constant MAX_CONTENT_LENGTH = 6000;
+    uint16 public MAX_CONTENT_LENGTH = 6000;
 
     struct Post {
         address author;
@@ -15,6 +16,15 @@ contract Blog {
     }
 
     mapping(address => Post[]) private posts; 
+
+    modifier onlyOwner {
+        require(owner == msg.sender, "Must be contract owner for this action");
+        _;
+    }
+
+    function changeContentLength(uint16 _newContentLength) public onlyOwner {
+        MAX_CONTENT_LENGTH = _newContentLength;
+    }
 
     function createPost(string memory _title, string memory _content) public {
         require(bytes(_title).length <= MAX_TITLE_LENGTH, "Title too long");
--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.18;
 
 contract Blog {
 
+    address public owner = msg.sender;
     uint8 constant MAX_TITLE_LENGTH = 128;
-    uint16 constant MAX_CONTENT_LENGTH = 6000;
+    uint16 public MAX_CONTENT_LENGTH = 6000;
 
     struct Post {
+        uint256 id;
         address author;
         string title;
         string content;
@@ -16,10 +18,20 @@ contract Blog {
 
     mapping(address => Post[]) private posts; 
 
+    modifier onlyOwner {
+        require(owner == msg.sender, "Must be contract owner for this action");
+        _;
+    }
+
+    function changeContentLength(uint16 _newContentLength) public onlyOwner {
+        MAX_CONTENT_LENGTH = _newContentLength;
+    }
+
     function createPost(string memory _title, string memory _content) public {
         require(bytes(_title).length <= MAX_TITLE_LENGTH, "Title too long");
         require(bytes(_content).length <= MAX_CONTENT_LENGTH, "Content too long");
         Post memory newPost = Post({
+            id: posts[msg.sender].length,
             author: msg.sender,
             title: _title,
             content: _content,
@@ -28,6 +40,17 @@ contract Blog {
         });
 
         posts[msg.sender].push(newPost);
+    }
+
+    function likePost(address _postAuthor, uint256 _postId) external {
+        require(posts[_postAuthor][_postId].id == _postId, "Post does not exist");
+        posts[_postAuthor][_postId].likes += 1;
+    }
+
+    function unlikeTweet(address _postAuthor, uint256 _postId) external {
+        require(posts[_postAuthor][_postId].id == _postId, "Post does not exist");
+        require(posts[_postAuthor][_postId].likes > 0, "Can't have negative likes");
+        posts[_postAuthor][_postId].likes -= 1;
     }
 
     function getPost(uint _i) public view returns (Post memory) {
